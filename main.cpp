397ee4fb0ba3392dbadf1d81ecadf52c3fb94246
eiginte <e.smigelskyte@gmail.com>
2025-09-25 12:57:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using std::cout;
 using std::cin;
@@ -21,10 +22,13 @@ struct Studentas {
     string pav;
     vector<int> paz;
     int egzas;
-    float rez;
+    double vid;
+    double med;
 };
 
 Studentas ivesk();
+double sk_med(vector<int> paz);
+
 
 int main() {
     vector<Studentas> Grupe;
@@ -33,9 +37,38 @@ int main() {
         Grupe.push_back(ivesk());
     }
 
-    cout << left << setw(15) << "Vardas:" << " | " << setw(20) <<"Pavarde" << " | "<< setw(10) << "Vidurkis:" << endl;
-    for (auto temp : Grupe)
-        cout << left << setw(15) << temp.vard << " | "<< setw(20) << temp.pav << " | "<< setw(10) << fixed << setprecision(2) << temp.rez << endl;
+
+    cout << "Ka norite isvesti?" << endl;
+    cout << "1 - Vidurki" << endl;
+    cout << "2 - Mediana" << endl;
+    cout << "3 - Abu (vidurki ir mediana)" << endl;
+    cout << "Pasirinkite:" << endl;
+    int pasirinkimas;
+    cin >> pasirinkimas;
+   
+
+
+    cout << left << setw(15) << "Vardas:" << " | " << setw(20) << "Pavarde" << " | "; 
+    if (pasirinkimas == 1)
+        cout << "Vidurkis: ";
+    else if (pasirinkimas == 2)
+        cout << "Mediana: ";
+    else
+        cout << "Vidurkis|Mediana";
+    cout << endl;
+
+    for (auto temp : Grupe) {
+
+        cout << left << setw(15) << temp.vard << " | " << setw(20) << temp.pav << " | ";
+        if (pasirinkimas == 1)
+            cout << fixed << setprecision(2) << temp.vid;
+        else if (pasirinkimas == 2)
+            cout << fixed << setprecision(2) << temp.med;
+        else
+            cout << fixed << setprecision(2) << temp.vid << " | " << fixed << setprecision(2) << temp.med;
+        
+        cout << endl;
+    }
 }
 
 
@@ -48,18 +81,36 @@ Studentas ivesk()
     cin >> Laik.vard;
     cout << "Ivesk pavarde: ";
     cin >> Laik.pav;
-    cout << "Kiek pazymiu turi studentas? ";
-    cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        cout << "Iveskite " << i + 1 << " paz. is " << n << " : ";
-        cin >> m;
-        Laik.paz.push_back(m);
-        sum += m;
-    }
+    cout << "Iveskite namu darbu pazymius (iveskite 0, jei norite baigti n.d. ivedima)"<< endl;
+        while (true) {
+            cin >> m;
+            if (m == 0) break;
+            Laik.paz.push_back(m);
+            sum += m;
+        }
+
     cout << "Iveskite egzamina: ";
     cin >> Laik.egzas;
-    Laik.rez = Laik.egzas * 0.6 + double(sum) / double(Laik.paz.size()) * 0.4;
+
+    if (!Laik.paz.empty()) {
+        Laik.vid = Laik.egzas * 0.6 + double(sum) / double(Laik.paz.size()) * 0.4;
+        Laik.med = Laik.egzas * 0.6 + sk_med(Laik.paz) * 0.4;
+    }
+    else {
+        Laik.vid = Laik.egzas * 0.6;
+        Laik.med = Laik.egzas * 0.6;
+    }
+
     return Laik;
 }
 
+double sk_med(vector<int> paz) {
+    sort(paz.begin(), paz.end());
+    size_t dydis = paz.size();
+
+    if (dydis % 2 == 0)
+        return (paz[dydis / 2 - 1] + paz[dydis / 2]) / 2.0;
+    else
+        return paz[dydis / 2];
+}
