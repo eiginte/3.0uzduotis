dc41417c30283db5d15ed8e9f6daeb6350c5e5ba
eiginte <e.smigelskyte@gmail.com>
2025-09-25 20:27:43 +0300

    Update main.cpp
    Galima pasirinkti, ar norime studentus įvesti patys, ar sugeneruoti juos, ar nuskaityti iš failo. Taip pat galima pasirinkti, ar norime matyti vidurkį, ar medianą, ar abu. Dalis pirmųjų rezultatų išvedami komandinėje eilutėje, pagal vardus abėcėlės tvarka. Visi rezultatai pagal abėcėlę yra išvedami į atskirą failą "rezultatai.txt".

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <sstream>
 #include <windows.h>
+#include <random>
 
 using std::cout;
 using std::cin;
@@ -20,8 +21,10 @@ using std::fixed;
 using std::ifstream;
 using std::istringstream;
 using std::ofstream;
-
-
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::stringstream;
 
 
 struct Studentas {
@@ -35,7 +38,188 @@ struct Studentas {
 
 
 double sk_med(vector<int> paz);
+vector<Studentas> skaitymas(const string& filename);
+Studentas ivesk();
 void irasymas_i_faila(const vector<Studentas>& Grupe, const string& failo_vardas);
+Studentas sugeneruoti_studento(int nd_max_sk = 5);
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    vector<Studentas> Grupe;
+
+    cout << "Pasirinkite, kaip norite gauti studentu duomenis:" << endl;
+    cout << "1 - Įvesti studentus patiems" << endl;
+    cout << "2 - Sugeneruoti atsitiktinius balus" << endl;
+    cout << "3 - Nuskaityti duomenis iš failo" << endl;
+    cout << "Pasirinkite: ";
+    int pradzios_pasirinkimas;
+    cin >> pradzios_pasirinkimas;
+
+    if(pradzios_pasirinkimas == 1) {
+        char dar;
+        do {
+            Grupe.push_back(ivesk());
+            cout << "Norite įvesti dar viena studentą? (t/n): ";
+            cin >> dar;
+        } while(dar == 't' || dar == 'T');
+    }
+    else if(pradzios_pasirinkimas == 2) {
+        int kiek;
+        cout << "Kiek studentu sugeneruoti? ";
+        cin >> kiek;
+        cout <<"Kiek namu darbu generuoti kiekvienam studentui?";
+        int nd_sk;
+        cin >> nd_sk;
+        for(int i = 0; i < kiek; ++i)
+            Grupe.push_back(sugeneruoti_studento(nd_sk));
+    }
+    else if(pradzios_pasirinkimas == 3) {
+        string failo_vardas;
+        cout << "Įveskite failo pavadinimą: ";
+        cin >> failo_vardas;
+        Grupe = skaitymas(failo_vardas);
+        if(Grupe.empty()){
+            cout << "Tuscias failas arba klaida" << endl;
+            return 1;
+        }
+    }
+    else {
+        cout << "Netinkamas pasirinkimas." << endl;
+        return 1;
+    }
+
+    sort(Grupe.begin(), Grupe.end(), [](const Studentas& a, const Studentas& b){
+        return a.vard < b.vard;
+    });
+
+    cout << "\nKa norite isvesti?" << endl;
+    cout << "1 - Vidurki" << endl;
+    cout << "2 - Mediana" << endl;
+    cout << "3 - Abu (vidurki ir mediana)" << endl;
+    cout << "Pasirinkite: ";
+    int pasirinkimas;
+    cin >> pasirinkimas;
+
+    cout << left << setw(15) << "Vardas" << " | " << setw(20) << "Pavarde" << " | ";
+    if (pasirinkimas == 1)
+        cout << setw(10)<< "Vidurkis: ";
+    else if (pasirinkimas == 2)
+        cout << setw(10) << "Mediana: ";
+    else
+        cout << setw(10) << "Vidurkis" << " | " << setw(10) << "Mediana";
+    cout << endl;
+    cout << string(70, '-') << endl;
+
+    int kiek = 0;
+    for (auto temp : Grupe) {
+        cout << left << setw(15) << temp.vard << " | " << setw(20) << temp.pav << " | ";
+        if (pasirinkimas == 1)
+            cout << setw(10) << fixed << setprecision(2) << temp.vid;
+        else if (pasirinkimas == 2)
+            cout << setw(10) << fixed << setprecision(2) << temp.med;
+        else
+            cout << setw(10) << fixed << setprecision(2) << temp.vid << " | "
+                 << setw(10) << fixed << setprecision(2) << temp.med;
+        cout << endl;
+        if (++kiek == 20) break;
+    }
+
+    irasymas_i_faila(Grupe, "rezultatai.txt");
+}
+
+void irasymas_i_faila(const vector<Studentas>& Grupe, const string& failo_vardas) {
+    ofstream out(failo_vardas);
+    if (!out) {
+        cout << "Nepavyko sukurti failo: " << failo_vardas << endl;
+        return;
+    }
+
+    out << left << setw(15) << "Vardas" << " | " << setw(20) << "Pavarde" << " | "
+        << setw(10) << "Vidurkis" << " | " << setw(10) << "Mediana" << endl;
+    out << string(60, '-') << endl;
+
+    for (auto &temp : Grupe) {
+        out << left << setw(15) << temp.vard << " | "
+            << setw(20) << temp.pav << " | "
+            << setw(10) << fixed << setprecision(2) << temp.vid << " | "
+            << setw(10) << fixed << setprecision(2) << temp.med << endl;
+    }
+
+    cout << "Rezultatai issaugoti faile " << failo_vardas << endl;
+}
+
+
+Studentas ivesk() {
+    Studentas Laik;
+    int sum = 0, m;
+    cout << "Ivesk varda: ";
+    cin >> Laik.vard;
+    cout << "Ivesk pavarde: ";
+    cin >> Laik.pav;
+
+    cout << "Iveskite namu darbu pazymius (iveskite 0, jei norite baigti n.d. ivedima)" << endl;
+    while (true) {
+        cin >> m;
+        if (m == 0) break;
+        Laik.paz.push_back(m);
+        sum += m;
+    }
+
+    cout << "Iveskite egzamina: ";
+    cin >> Laik.egzas;
+
+    if (!Laik.paz.empty()) {
+        Laik.vid = Laik.egzas * 0.6 + double(sum) / double(Laik.paz.size()) * 0.4;
+        Laik.med = Laik.egzas * 0.6 + sk_med(Laik.paz) * 0.4;
+    }
+    else {
+        Laik.vid = Laik.egzas * 0.6;
+        Laik.med = Laik.egzas * 0.6;
+    }
+
+    return Laik;
+}
+
+Studentas sugeneruoti_studento(int nd_sk) {
+    static int nr = 1;
+    Studentas Laik;
+
+    stringstream ss;
+    ss << nr;
+    Laik.vard = "Vardas" + ss.str();
+    Laik.pav = "Pavarde" + ss.str();
+    ++nr;
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> paz_dist(1, 10);
+    uniform_int_distribution<> egz_dist(1, 10);
+
+
+    for(int i = 0; i < nd_sk; ++i)
+        Laik.paz.push_back(paz_dist(gen));
+
+    Laik.egzas = egz_dist(gen);
+
+    int sum = 0;
+    for(auto x : Laik.paz) sum += x;
+    Laik.vid = Laik.egzas * 0.6 + double(sum)/Laik.paz.size()*0.4;
+    Laik.med = Laik.egzas * 0.6 + sk_med(Laik.paz)*0.4;
+
+    return Laik;
+}
+
+double sk_med(vector<int> paz) {
+    sort(paz.begin(), paz.end());
+    size_t dydis = paz.size();
+    if (dydis % 2 == 0)
+        return (paz[dydis / 2 - 1] + paz[dydis / 2]) / 2.0;
+    else
+        return paz[dydis / 2];
+}
+
 
 vector<Studentas> skaitymas(const string& filename) {
     vector<Studentas> Grupe;
@@ -66,7 +250,6 @@ vector<Studentas> skaitymas(const string& filename) {
 
         s.egzas = visiPaz.back();
         visiPaz.pop_back();
-
         s.paz = visiPaz;
 
         if (!s.paz.empty()) {
@@ -83,126 +266,4 @@ vector<Studentas> skaitymas(const string& filename) {
     }
 
     return Grupe;
-}
-
-int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
-
-    string failo_vardas = "studentai10000.txt";
-    vector<Studentas> Grupe = skaitymas(failo_vardas);
-
-    if(Grupe.empty()){
-        cout << "Tuscias failas arba klaida" << endl;
-        return 1;
-    }
-
-    sort(Grupe.begin(),Grupe.end(), [](const Studentas& a, const Studentas&b){
-         return a.vard<b.vard;
-         });
-
-    cout << "Ka norite isvesti?" << endl;
-    cout << "1 - Vidurki" << endl;
-    cout << "2 - Mediana" << endl;
-    cout << "3 - Abu (vidurki ir mediana)" << endl;
-    cout << "Pasirinkite:" << endl;
-    int pasirinkimas;
-    cin >> pasirinkimas;
-
-
-
-    cout << left << setw(15) << "Vardas:" << " | " << setw(20) << "Pavarde" << " | ";
-    if (pasirinkimas == 1)
-        cout << setw(10)<< "Vidurkis: ";
-    else if (pasirinkimas == 2)
-        cout << setw(10) << "Mediana: ";
-    else
-        cout << setw(10) << "Vidurkis" << " | " << setw(10) << "Mediana";
-    cout << endl;
-    cout << string(70, '-') << endl;
-
-    int kiek = 0;
-    for (auto temp : Grupe) {
-
-        cout << left << setw(15) << temp.vard << " | " << setw(20) << temp.pav << " | ";
-        if (pasirinkimas == 1)
-            cout << setw(10) << fixed << setprecision(2) << temp.vid;
-        else if (pasirinkimas == 2)
-            cout << setw(10) << fixed << setprecision(2) << temp.med;
-        else
-            cout << setw(10) << fixed << setprecision(2) << temp.vid << " | " << setw(10) << fixed << setprecision(2) << temp.med;
-
-
-        cout << endl;
-        if (++kiek == 20) break;
-    }
-
-    irasymas_i_faila(Grupe, "rezultatai.txt");
-
-}
-
-
-
-Studentas ivesk()
-{
-    Studentas Laik;
-    int sum = 0, n, m;
-    cout << "Ivesk varda: ";
-    cin >> Laik.vard;
-    cout << "Ivesk pavarde: ";
-    cin >> Laik.pav;
-
-    cout << "Iveskite namu darbu pazymius (iveskite 0, jei norite baigti n.d. ivedima)"<< endl;
-        while (true) {
-            cin >> m;
-            if (m == 0) break;
-            Laik.paz.push_back(m);
-            sum += m;
-        }
-
-    cout << "Iveskite egzamina: ";
-    cin >> Laik.egzas;
-
-    if (!Laik.paz.empty()) {
-        Laik.vid = Laik.egzas * 0.6 + double(sum) / double(Laik.paz.size()) * 0.4;
-        Laik.med = Laik.egzas * 0.6 + sk_med(Laik.paz) * 0.4;
-    }
-    else {
-        Laik.vid = Laik.egzas * 0.6;
-        Laik.med = Laik.egzas * 0.6;
-    }
-
-    return Laik;
-}
-
-void irasymas_i_faila(const vector<Studentas>& Grupe, const string& failo_vardas) {
-    ofstream out(failo_vardas);
-    if (!out) {
-        cout << "Nepavyko sukurti failo: " << failo_vardas << endl;
-        return;
-    }
-
-    out << left << setw(15) << "Vardas" << " | " << setw(20) << "Pavarde" << " | "
-        << setw(10) << "Vidurkis" << " | " << setw(10) << "Mediana" << endl;
-    out << string(60, '-') << endl;
-
-    for (auto &temp : Grupe) {
-        out << left << setw(15) << temp.vard << " | "
-            << setw(20) << temp.pav << " | "
-            << setw(10) << fixed << setprecision(2) << temp.vid << " | "
-            << setw(10) << fixed << setprecision(2) << temp.med << endl;
-    }
-
-    cout << "Rezultatai issaugoti faile " << " '"<< failo_vardas << "' "<< endl;
-}
-
-
-double sk_med(vector<int> paz) {
-    sort(paz.begin(), paz.end());
-    size_t dydis = paz.size();
-
-    if (dydis % 2 == 0)
-        return (paz[dydis / 2 - 1] + paz[dydis / 2]) / 2.0;
-    else
-        return paz[dydis / 2];
 }
