0f735b22bdab2f756ccf82f93e97045e20a0906f
eiginte <e.smigelskyte@gmail.com>
2025-12-10 19:32:17 +0200

    Add files via upload

--- html/navtreedata.js
+++ html/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "My Project", "index.html", [
+  [ "Objektinio programavimo darbas, studentų rūšiavimas", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['doxyfile_0',['Doxyfile',['../md__doxyfile.html',1,'']]]
+];
